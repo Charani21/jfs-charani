a5c25ce646629dd76fdc4c30528c067a328b827e
Charani21 <charani91004@gmail.com>
2024-01-25 00:54:28 -0500

    Initial commit

---
+++ src/main/java/com/careerit/jfs/day1/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.careerit.jfs.day1;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java World");
+    }
+
+}
+
